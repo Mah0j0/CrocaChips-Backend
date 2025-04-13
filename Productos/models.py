90ae0f3594b805a55b3126ca6fe992a429280a5e
gabriel-arinez <gabrielarinez056@gmail.com>
2025-04-13 13:43:09 -0400

    Definición del serializador

--- Productos/models.py
+++ Productos/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Producto(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=255)
+    descripcion = models.CharField(max_length=255)
+    tiempo_vida = models.IntegerField()
+    precio_unitario = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        db_table = 'productos'
+
+    def __str__(self):
+        return self.nombre
