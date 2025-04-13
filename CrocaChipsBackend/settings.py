9b9269f4bdc2c616e934a76c1b2d03151f113e0b
gabriel-arinez <gabrielarinez056@gmail.com>
2025-04-13 12:59:20 -0400

    Creación del módulo 'Productos' y sus archivos

--- CrocaChipsBackend/settings.py
+++ CrocaChipsBackend/settings.py
@@ -102,11 +102,11 @@ WSGI_APPLICATION = 'CrocaChipsBackend.wsgi.app'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  
-        'NAME': os.getenv('DB_NAME'),         
-        'USER': os.getenv('DB_USER'),         
-        'PASSWORD': os.getenv('DB_PASSWORD'),  
-        'HOST': os.getenv('DB_HOST'),  
-        'PORT': os.getenv('DB_PORT'), 
+        'NAME': 'bd_croca_chips',         
+        'USER': 'root',         
+        'PASSWORD': 'tArtcqaAiRUuFxZvUTiqjRNHYagEEdtR',  
+        'HOST': 'maglev.proxy.rlwy.net',  
+        'PORT': '43380', 
     }
 }
 
