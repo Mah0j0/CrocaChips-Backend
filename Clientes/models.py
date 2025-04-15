a70437d071ea70ebedf8e4344e79cf7e0ecde883
gabriel-arinez <gabrielarinez056@gmail.com>
2025-04-15 13:47:13 -0400

    Definición del Modelo Clientes

--- Clientes/models.py
+++ Clientes/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Cliente(models.Model):
+    id_cliente = models.AutoField(primary_key=True, db_column='id_cliente')
+    nombre = models.CharField(max_length=30)
+    direccion = models.CharField(max_length=255)
+    telefono = models.IntegerField()
+    habilitado = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    
+    class Meta:
+        db_table = 'clientes'
+    
+    def __str__(self):
+        return self.nombre
