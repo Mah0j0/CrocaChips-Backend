26a30d44613014383151dfb6194a57ed17949519
Henrry <148930610+HenrryDG@users.noreply.github.com>
2025-04-15 18:05:26 -0400

    Cambiando el envio de id del producto por el body para la eliminación y actualización (PUT y DELETE)

--- Productos/urls.py
+++ Productos/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('productos/', lista_productos),
     path('productos/<int:id_producto>/', producto),
     path('productos/registrar/', registrar_producto),
-    path('productos/actualizar/<int:id_producto>/', actualizar_producto),
-    path('productos/eliminar/<int:id_producto>/', eliminar_producto),
+    path('productos/actualizar/', actualizar_producto),
+    path('productos/eliminar/', eliminar_producto),
 ]
