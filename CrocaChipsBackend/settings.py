d7de8695642521505696bef26240b1d341c3e6bb
Henrry <148930610+HenrryDG@users.noreply.github.com>
2025-04-17 21:25:06 -0400

    Agregando la aplicación de Ventas a la configuración del proyecto y creación del modelo y serializador para gestionar las ventas.

--- CrocaChipsBackend/settings.py
+++ CrocaChipsBackend/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'Productos',
     'Clientes',
     'MovimientosAlmacen',
+    'Ventas',
 
     # Librerias
     'corsheaders',
--- Ventas/models.py
+++ Ventas/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from Empleados.models import Empleado
+from Clientes.models import Cliente
 
-# Create your models here.
+class Venta(models.Model):
+    id_venta = models.AutoField(primary_key=True, db_column='id_venta')
+    vendedor = models.ForeignKey(Empleado, on_delete=models.CASCADE, db_column='id_vendedor', related_name='ventas')
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, db_column='id_cliente', related_name='ventas')
+    estado = models.BooleanField(default=True)  # True = Activo, False = Inactivo
+    fecha = models.DateField(auto_now_add=True)
+    precio_total = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        db_table = 'ventas'
+
+        def __str__(self):
+            return f'Venta {self.id_venta} - {self.vendedor} - {self.cliente} - {self.fecha}'
